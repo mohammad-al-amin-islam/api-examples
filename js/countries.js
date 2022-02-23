c4a296d75f4fee8352a29027b8c283d1201793d4
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-23 12:15:23 +0600

    country details dynamicly practice added

---
+++ js/countries.js
@@ -0,0 +1,44 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v2/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+loadCountries();
+const displayCountries = (countries) => {
+    const countriesHolder = document.getElementById('countries');
+    countries.forEach(country => {
+        const div = document.createElement('div');
+        div.classList.add('country');
+        /* const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        console.log(country);
+        h3.innerText = country.name;
+        p.innerText = country.capital;
+
+        div.appendChild(h3);
+        div.appendChild(p);
+        */
+        div.innerHTML = `
+       <h3>${country.name}</h3>
+       <p>${country.capital}</p>
+       <button onclick='displayCountryDetails("${country.name}")'>Details</button>
+       `
+        countriesHolder.appendChild(div);
+    })
+}
+const displayCountryDetails = (name) => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayInfo(data[0]))
+}
+
+const displayInfo = data =>{
+    const detailsDiv = document.getElementById('country-details');
+    detailsDiv.innerHTML =`
+        <h5>name: ${data.name.common}</h5>
+        <p>population: ${data.population}</p>
+        <img src="${data.flags.svg}"/>
+    `
+    detailsDiv.style.textAlign = 'center';
+}
